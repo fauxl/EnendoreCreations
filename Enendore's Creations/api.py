0a02c9ee18351e1cb05b2aee9f702700747b2b2e
Gerardo De Rosa <duckpro@libero.it>
2020-09-28 22:43:06 +0200

    Frist Update
    Frist Update

---
+++ Enendore's Creations/api.py
@@ -0,0 +1,16 @@
+import  flask
+from flask import Flask, jsonify, render_template
+from pymysql.constants.FIELD_TYPE import JSON
+
+app = Flask(__name__)
+
+@app.route("/index")
+def index():
+    return render_template("Index.html")
+
+@app.route("/prodotto")
+def prodotto():
+    return render_template("prodotto.html")
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
